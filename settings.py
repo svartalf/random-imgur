bee85f7bfd47a312a7b3bdc6beca17bfa1003eac
SvartalF <self@svartalf.info>
2012-02-27 19:15:31 +0900

    Checker interval decreased to one second

--- settings.py
+++ settings.py
@@ -7,6 +7,6 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 REDIS_DATABASE = 1
 
 # Check image status in seconds
-CHECKER_INTERVAL = 3
+CHECKER_INTERVAL = 1
 
 TEMPLATE_PATH = os.path.join(PROJECT_ROOT, 'templates')
